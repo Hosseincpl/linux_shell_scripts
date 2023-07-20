c7142a895acc0937cdc4dc805af59ae208ab32e0
Hossein Karami <72271858+Hosseincpl@users.noreply.github.com>
2023-07-20 08:45:42 +0330

    Update stlink.sh

--- stlink.sh
+++ stlink.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 ls -ltrha
+
+# st-link version 
+st-info --version
+
 # finding connected st-link drivers
 st-info --probe
+
+# Reading & Writing Option Bytes (on SRAM)
+st-flash --reset read build/fileName.bin 0x08000000 4
+st-flash --reset write build/fileName.bin 0x08000000
